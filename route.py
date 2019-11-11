840f15c1502c81333df809ad3bf21023431d57f0
nahuel <debellisnahuel@gmail.com>
2019-11-10 23:29:36 -0300

    change port (80)

--- route.py
+++ route.py
@@ -23,7 +23,7 @@ def semantic():
     return response
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = 80 #int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
 
 
